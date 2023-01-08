2bef5f352a6f58c8d92e55f68d1dd185333cc7ad
alondramora <mora_ac@yahoo.com>
2023-01-08 13:34:04 -0800

    fixed connection string by adding DB password

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-const express = require('express'); 
+//declare variables
+const express = require('express');
+const app = express(); // we assign express to a variable called app for easier reference
+const PORT = 8000;
+const mongoose = require('mongoose'); // allows us to talk to the mongo database in an easier way
+
+//set middleware
+app.set('view engine', 'ejs'); // setting the view engine, this is built into express
+// we don't have to use ejs for our view engine, we can use things like react as well
+app.set(express.static('public')); // allows us to put static files in a folder called public and tells express to look in that folder for said static files
+app.use(express.urlencoded({ extended: true })); // making sure we can parse requests from forms and post requests into our app
