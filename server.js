cf43afbac6c8df305f4b7898c13680fa95addb96
alondramora <mora_ac@yahoo.com>
2023-01-08 14:38:39 -0800

    created index views page

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
-//declare variables
+//Declare Variables
+const app = require('express');
 const express = require('express');
 const app = express(); // we assign express to a variable called app for easier reference
 const PORT = 8000;
 const mongoose = require('mongoose'); // allows us to talk to the mongo database in an easier way
 
-//set middleware
+//Set Middleware
 app.set('view engine', 'ejs'); // setting the view engine, this is built into express
 // we don't have to use ejs for our view engine, we can use things like react as well
 app.set(express.static('public')); // allows us to put static files in a folder called public and tells express to look in that folder for said static files
 app.use(express.urlencoded({ extended: true })); // making sure we can parse requests from forms and post requests into our app
+
+// Start Server
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`)); // listening for any input from the user
